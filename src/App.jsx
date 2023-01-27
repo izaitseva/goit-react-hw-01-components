1f56f67a86a9aa47620e084d692b3273b02ecab8
Iryna Zaitseva <zaitseva02@gmail.com>
2023-01-27 22:03:42 +0200

    1 task

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './index.css';
+import { Profile } from 'components/Profile';
+import user from "./user.json";
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+};
+
