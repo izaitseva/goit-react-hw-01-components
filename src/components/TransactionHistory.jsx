f1be3c7a5d860a107ea8a9416961abf6df4ca1a5
Iryna Zaitseva <zaitseva02@gmail.com>
2023-01-29 22:08:50 +0200

    task 4

--- src/components/TransactionHistory.jsx
+++ src/components/TransactionHistory.jsx
@@ -1,2 +1,24 @@
 
-export const TransactionHistory = "here";
+export const TransactionHistory = ({ items }) => {
+    return (
+        <table class="transaction-history">
+            <thead>
+                <tr>
+                    <th>Type</th>
+                    <th>Amount</th>
+                    <th>Currency</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {items.map(trans => (
+                <tr>
+                    <td>{trans.type}</td>
+                    <td>{trans.amount}</td>
+                    <td>{trans.currency}</td>
+                </tr>
+                ))}
+            </tbody>
+        </table>
+    )
+};
