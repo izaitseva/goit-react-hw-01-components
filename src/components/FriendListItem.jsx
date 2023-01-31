ffbf37ac39c7fcdb4d18461dd033303eb50dacd6
Iryna Zaitseva <zaitseva02@gmail.com>
2023-01-31 17:51:06 +0200

    props transitions

--- src/components/FriendListItem.jsx
+++ src/components/FriendListItem.jsx
@@ -1,11 +1,20 @@
+import PropTypes from "prop-types";
+
 import { Ident, FriendsLi, Avatar } from "./FriendList.styled"
 
 export const FriendListItem = ({avatar, name, isOnline}) => {
     return (
         <FriendsLi>
-            <Ident isOnline={isOnline}>{isOnline}</Ident>
+            <Ident isOnline={isOnline}></Ident>
             <Avatar src={avatar} alt="User avatar" />
             <p className="name" >{name}</p>
         </FriendsLi>
     )
 }
+
+FriendListItem.propTypes = {
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    isOnline: PropTypes.bool.isRequired,
+}
+
